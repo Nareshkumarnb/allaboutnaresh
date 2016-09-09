ed53f86faba65ce20923701d35bb3b68afa2c0b1
Nareshkumarnb <hseran16@gmail.com>
2016-09-09 09:40:21 +0530

    init moved

---
+++ codingfeat.com/wpthemes/ena/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://codingfeat.com/wpthemes/ena</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://codingfeat.com/wpthemes/ena/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://codingfeat.com/wpthemes/ena/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://codingfeat.com/wpthemes/ena/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://codingfeat.com/wpthemes/ena/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://codingfeat.com/wpthemes/ena/?rest_route=/" />
+	    </apis>
+  </service>
+</rsd>
